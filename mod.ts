6bd187d7663eaf744858e1794885f2cad71075e3
Kjartan Hrafnkelsson <kjartan@kjartan.io>
2024-05-21 22:43:24 +0000

    add example

--- mod.ts
+++ mod.ts
@@ -2,5 +2,11 @@
  * Silly alias for console.log.
  * 
  * @alias console.log
+ * 
+ * ```ts
+ * import { printf } from "@femboy/printf";
+ *
+ * printf("testing", "123");
+ * ```
  */
 export const printf = console.log;
